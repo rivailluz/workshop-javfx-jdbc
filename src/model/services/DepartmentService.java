158ba928728c4960fe2f9b040e2f1db1f5069fa2
rivailluz <rivailluz@gmail.com>
2019-04-29 20:19:04 -0400

    Saving a new Department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -13,4 +13,13 @@ public class DepartmentService {
 	public List<Department> findAll(){
 		return dao.findAll();
 	}
+	
+	public void saveOrUptade(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 }
